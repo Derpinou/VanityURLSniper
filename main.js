4de045c315e1066d5564a4301c376d394cd724c0
Derp <60037058+Derpinou@users.noreply.github.com>
2020-11-23 18:13:48 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,8 @@
-const Discord = require('discord.js')
-const client = new Discord.Client()
-const moment = require("moment-timezone");
-
+const Discord = require('discord.js'),
+      client = new Discord.Client(),
+      moment = require("moment-timezone");
 /*
     D0wzy - 2020
-
     Website: https://dowzy.fr/
     GitHub: https://github.com/D0wzy/
     VoltBot: https://voltbot.xyz/
@@ -16,7 +14,7 @@ class Main {
 
     async setVanityURL(url, guild) {
         const time = moment.tz(Date.now(), "Europe/Paris").format("HH:mm:ss");
-        console.log(`[${time}] [INFO] Sniping discord.gg/${url}`)
+        console.log(`[${time}] [INFO] Sniping discord.gg/${url}`);
         return await fetch(`https://discord.com/api/v8/guilds/${guild.id}/vanity-url`, {
             "credentials": "include",
             "headers": {
@@ -48,39 +46,40 @@ class Main {
 
     startSnipe(url, guild) {
         this.sniperInterval = setInterval(async () => {
-            this.setVanityURL(url, guild)
-        }, 1000)
+            this.setVanityURL(url, guild);
+        }, 1000);
     }
 
     stopSnipe() {
-        return clearInterval(this.sniperInterval)
+        return clearInterval(this.sniperInterval);
     }
 }
-const prefix = "!"
+const prefix = "!";
 
-let handler = new Main()
+let handler = new Main();
 
 client.on('message', async (message) => {
-    let messageArray = message.content.split(" ");
-    const args1 = message.content.slice(prefix.length).split(/ +/);
-    const command = args1.shift().toLowerCase();
-    let args = messageArray.slice(1);
+    let messageArray = message.content.split(" "),
+        args = messageArray.slice(1);
+    const args1 = message.content.slice(prefix.length).split(/ +/),
+          command = args1.shift().toLowerCase();
 
     if (command === "start-snipe") {
-        let url = args[0]
-        handler.startSnipe(url)
+        let url = args[0];
+        handler.startSnipe(url);
 
         if (!message.guild.features.includes('VANITY_URL')) {
-            return message.reply("Vous ne possedez pas l'options VANITY_URL")
-        }
+            return message.reply("Vous ne possedez pas l'options VANITY_URL");
+        };
 
-        message.reply(`Je commance à snipe l'URL ${url} dés maintenant`)
+        message.reply(`Je commence à snipe l'URL ${url} dés maintenant`);
         console.log(`[INFO] Start sniping the url ${url} !`);
-    }
+    };
 
     if (command === "stop-snipe") {
-        handler.stopSnipe()
-    }
+        handler.stopSnipe();
+    };
+    
 
-})
-client.login("Super secret token here")
+});
+client.login("Super secret token here");
